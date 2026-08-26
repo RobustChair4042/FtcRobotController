4b32f29b5cf5b816645b8c89fc7b9b189c5f45c2
RobustChair4042 <sabresprite@gmail.com>
2026-08-26 16:01:36 +0100

    Create VariableExamples.java
    Looked at some examples of variables

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+public class VariableExamples {
+
+    //Data Types
+
+    int robotHeight;
+    double motorSpeed;
+    boolean clawClosed;
+
+    robotHeight = 18;
+    motorSpeed = 0.3;
+    clawClosed = true;
+
+
+}
