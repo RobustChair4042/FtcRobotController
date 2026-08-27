e609cf72ec1c7b273c815d151ea7774c95aecd74
RobustChair4042 <sabresprite@gmail.com>
2026-08-27 11:02:26 +0100

    Create VariablePractice.java
    Practiced creating more variable as tried doing some by myself without the tutorial.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariablePractice extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 22593;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        int motorAngle = 164;
+        String coachName = "Mr Rhee + Mr Munube";
+        String teamName = "Drakon";
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Motor Angle", motorAngle);
+        telemetry.addData("Claw Closed", clawClosed);
+        telemetry.addData("Coaches Name", coachName);
+        telemetry.addData("Team Name", teamName);
+    }
+
+    @Override
+    public void loop() {
+
+
+    }
+}
