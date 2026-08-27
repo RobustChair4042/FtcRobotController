ec739d85173ca26d0c4a18b0f891c3df7a8a3179
RobustChair4042 <sabresprite@gmail.com>
2026-08-27 10:32:25 +0100

    Update VariableExamples.java
    Added some comments so that I could look back on the different types of variables.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
@@ -5,8 +5,11 @@ public class VariableExamples {
     //Data Types
 
     int robotHeight;
+    // When you use the int, the value assigned to that variable must be an integer.
     double motorSpeed;
+    // When you use the double, the value assigned to that variable must be a decimal.
     boolean clawClosed;
+    // When you use a boolean, the value assigned to that variable must be either true or false.
 
     robotHeight = 18;
     motorSpeed = 0.3;
