19cbb353a8db3d1613ee1ccc7d619dc480247798
RobustChair4042 <sabresprite@gmail.com>
2026-08-28 10:54:50 +0100

    Update RobotLocationPractice.java
    As I said before I needed to finish the tutorial video so I updated the file to create getter, sett and change functions so that I could then use them in UseRobotLocationOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLocationPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLocationPractice.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 public class RobotLocationPractice {
 
     double angle;
+    double x;
+    double y;
 
     //constructor method
     public RobotLocationPractice(double angle) {
@@ -24,9 +26,45 @@ public class RobotLocationPractice {
             angle += 360; // Add until in target range
         }
         return angle; //Return in normalised value.
+
+    }
+
+    public void turnRobot(double angleChange) {
+        angle += angleChange;
     }
 
     public void setAngle(double angle) {
         this.angle = angle;
     }
+
+    public double getAngle() {
+        return this.angle;
+    }
+
+    public void changeX(double changeAmount) {
+        x += changeAmount;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void changeY(double changeAmount) {
+        y += changeAmount;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+
+
 }
