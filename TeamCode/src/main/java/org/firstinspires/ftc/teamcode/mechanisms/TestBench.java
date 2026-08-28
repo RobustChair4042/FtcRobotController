f632c32164a47377008d8ce6c23483b46f6445a1
RobustChair4042 <sabresprite@gmail.com>
2026-08-28 12:21:01 +0100

    Create TestBench.java
    Needs updating. Will update tomorrow. Have not finished tutorial on touch sensor.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TestBench {
+    private DigitalChannel touchSensor;
+
+
+    public void init(HardwareMap hwMap) {
+         touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
+     }
+}
